30c092c544250aec699d42ecd4f1bea8acf4f6d1
htglvl <htglvl@gmail.com>
2019-08-17 10:53:32 +0700

    xong manipulate time

---
+++ Phòng thí nghiệm ảo vật lý/Assets/Script/TimeBody.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class TimeBody : MonoBehaviour
+{
+    public bool isrewinding = false;
+    List<Vector3> positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopRewind();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartRewind();
+        }
+        if (GetComponent<TimeControlSystem>().timesclider >= 0)
+        {
+            StopRewind();
+        }
+        else
+        {
+            StartRewind();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isrewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+    void Record()
+    {
+        positions.Insert(0, transform.position);
+    }
+    void Rewind()
+    {
+        if (positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind(); 
+        }
+    }
+    public void StartRewind()
+    {
+        isrewinding = true;
+    }
+    public void StopRewind()
+    {
+        isrewinding = false;
+        GetComponent<TimeControlSystem>().TimeControl.value = 0;
+    }
+}
---
+++ Phòng thí nghiệm ảo vật lý/Assets/Script/TimeControlSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimeControlSystem : MonoBehaviour
+{
+    public TextMeshProUGUI timeword;
+    public float timesclider;
+    public Slider TimeControl;
+    List<Vector3> positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Vector3>();
+        timesclider = 1;
+        TimeControl.value = 1f;
+        timeword.text = timesclider.ToString();
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (timesclider != 0)
+            {
+                TimeControl.value = 0;
+            }
+            else
+            {
+                TimeControl.value = 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            TimeControl.value = -1;
+        }
+        timesclider = TimeControl.value;
+        timeword.text = timesclider.ToString();
+    }
+
+}
